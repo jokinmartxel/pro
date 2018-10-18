8b7c486503e55767a5f4cf52c056c5a0576a83d7
Jokin Garmendia <jokin746@gmail.com>
2018-10-18 12:33:49 +0200

    zuzenketak

--- php/showQuestions.php
+++ php/showQuestions.php
@@ -1,28 +1,23 @@
 <?php 
 
-//$id= $_GET['id'];
-//$zenb=0;
 include "dbConfig.php";
 global $niremysqli;
-$sql = "SELECT * FROM questions";
-$result = mysqli_query($niremysqli, $sql); 
 
-if (mysqli_num_rows($result) > 0) {
+$sql = "SELECT * FROM questions";
+$res = mysqli_query($niremysqli, $sql); 
+
+if (mysqli_num_rows($res) > 0) {
+	
 	echo "<table border = '1'> \n"; 
 	echo "<tr><td>ID</td><td>Emaila</td><td>Galdera</td><td>Erantzun zuzena</td><td>Erantzun okerra 1</td><td>Erantzun okerra 2</td><td>Erantzun okerra 3</td><td>Zailtasuna</td><td>Gaia</td></tr> \n"; 
-	while ($row = mysqli_fetch_assoc($result)){ 
+	while ($row = mysqli_fetch_assoc($res)){ 
 		   echo "<tr><td>" . $row["Id"]. "</td><td>" . $row["Email"]. "</td><td>" . $row["Galdera"]. "</td><td>" . $row["Zuzena"]. " </td><td>" . $row["Okerra1"]. "</td><td> " . $row["Okerra2"]. "</td><td>" . $row["Okerra3"]. "</td><td>" . $row["Zailtasuna"]. "</td><td>" . $row["Gaia"]. "</td></tr> \n"; 
 	}
 }
-else{
-	echo "0 lerro taulan";
-}
+else{ echo "Ez dago lerrorik";}
+if (!$res){	echo("Errorea query-a gauzatzerakoan: ". mysqli_error($niremysqli));}
 
-if (!$result)
-{
-echo("Errorea query-a gauzatzerakoan: ". mysqli_error($niremysqli));
-}
-echo('<a href="../layout.html"> HASIERAKO ORRIRA ITZULTZEKO HEMEN KLIKATU </a></br></br>');
+echo('<a href="../layout.html"> ITZULI HASIERAKO ORRIRA </a></br></br>');
 mysqli_close($niremysqli);
 
 ?>
