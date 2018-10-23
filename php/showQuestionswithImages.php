f82d79a60c0a361b9ad56a7e460f0281d50e36b5
Jokin Garmendia <jokin746@gmail.com>
2018-10-23 16:28:59 +0200

    patharekin

--- php/showQuestionswithImages.php
+++ php/showQuestionswithImages.php
@@ -11,7 +11,7 @@ if (mysqli_num_rows($res) > 0) {
 	echo "<table border = '1'> \n"; 
 	echo "<tr><td>ID</td><td>Emaila</td><td>Galdera</td><td>Erantzun zuzena</td><td>Erantzun okerra 1</td><td>Erantzun okerra 2</td><td>Erantzun okerra 3</td><td>Zailtasuna</td><td>Gaia</td><td>Irudia</td></tr> \n"; 
 	while ($row = mysqli_fetch_assoc($res)){ 
-		   echo "<tr><td>" . $row["Id"]. "</td><td>" . $row["Email"]. "</td><td>" . $row["Galdera"]. "</td><td>" . $row["Zuzena"]. " </td><td>" . $row["Okerra1"]. "</td><td> " . $row["Okerra2"]. "</td><td>" . $row["Okerra3"]. "</td><td>" . $row["Zailtasuna"]. "</td><td>" . $row["Gaia"]. "</td><td> <img width='250' height='250' src='" . $row["Irudia"] . "'/> </td></tr> \n"; 
+		   echo "<tr><td>" . $row["Id"]. "</td><td>" . $row["Email"]. "</td><td>" . $row["Galdera"]. "</td><td>" . $row["Zuzena"]. " </td><td>" . $row["Okerra1"]. "</td><td> " . $row["Okerra2"]. "</td><td>" . $row["Okerra3"]. "</td><td>" . $row["Zailtasuna"]. "</td><td>" . $row["Gaia"]. "</td><td> <img width='75' height='75' src='" . $row["Irudia"] . "'/> </td></tr> \n"; 
 	}
 }
 else{ echo "Ez dago lerrorik";}
