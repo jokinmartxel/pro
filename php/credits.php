7d64332f1398d6a964822ef63fa86472fefdb2a3
Martxel Eizaguirre <jokinmartxel@gmail.com>
2018-11-24 12:49:12 +0100

    2. hautazkoa bukatua. LAB 06 bukatua

--- php/credits.php
+++ php/credits.php
@@ -12,15 +12,80 @@
 		   media='only screen and (max-width: 480px)'
 		   href='../styles/smartphone.css' />
 	<script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js'></script>
-  </head>
-  <body>
+	
+	
+	
+	<script>
+		var lat ="";
+		var lon = "";
+		
+		// geolokalizatzeko ip_api web aplikazioa erabili da http://ip-api.com/
+		function geolokalizatu() {
+			$.getJSON("http://ip-api.com/json/?callback=?", function(data) {
+
+				var hiria = data.city;
+				var lurraldea = data.country;
+				var lat = data.lat;
+				var lon = data.lon;
+				var region = data.regionName;
+				var zipcode = data.zip;
+				var datuak = "Leku honetatik erabiltzen ari zara web aplikazioa: </br>" + hiria +", " + region + ", " + lurraldea + " Lat: "+ lat+ " Lon: " + lon ;
+				
+				$("#geoL").html(datuak);
+				
+				
+				maparatu(lat, lon);
+
+			});
+			
+		}
+		
+		// kokapena mapa batean bistaratzeko https://leafletjs.com/ erabili da Google Api ordainekoa delako
+		function maparatu(lat, lon){
+			var map = L.map('map').
+			setView([lat, lon], 
+			14);
+			 
+			L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+					maxZoom: 18
+			}).addTo(map);
+
+			L.control.scale().addTo(map);
+			L.marker([lat, lon], {draggable: true}).addTo(map);
+		}
+	
+	</script>
+	
+
+	
+	
+	
+	
+	
+  <script src="https://unpkg.com/leaflet@1.0.2/dist/leaflet.js"></script>
+	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.2/dist/leaflet.css">
+
+ <style>
+  #map { 
+  widh: 300px;
+  height: 300px; }
+ </style>
+ 
+ </head>
+  <body onload="geolokalizatu()">
 <center>
   <h1>Jokin Garmendia eta Martxel Eizaguirre</h1>
   <h2>Softare eta Konputazio espezialitateakoak</h2>
   <img src= "../images/pertsonak.jpg" alt="gu" width="300">
   <h3>Tolosa eta Hondarribia</h3>
+  
+  <div id="geoL"></div>
+  <div id="map" style="width:400px ;height:360px"></div>
   </center>
   <a href="layout.php" id="home">HOME</a>
+  
+  
+
 </body>
 </html>
 
