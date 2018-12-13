34d0433d731b92ee41958f464c02dee805844cca
Martxel Eizaguirre <jokinmartxel@gmail.com>
2018-12-13 11:19:01 +0100

    igot

--- php/oneplay.php
+++ php/oneplay.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -5,19 +9,35 @@
 	<title>OnePlay</title>
 
 	<script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js'></script>
+	<script language="javascript">
+		function konprobatu(){
+			if (($('input[name="erantzunak"]:checked').val()).localeCompare(zuzena)){
+				alert("Erantzun Zuzena");
+				
+			}else{
+				alert("Erantzun Okerra");
+			}
+			var option = confirm("Beste galdera bat nahi duzu?");
+			if (option == true){
+				location.href="oneplay.php";				
+			}else{
+				location.href="layout.php";
+			}
+			
+		}
+	</script>
   </head>
   <body>
 
-	<form id="oneplay" method="post" action="zuzendu.php">
+	<form id="oneplay" method="post" action="">
 				Galdera: <input id="galdera" name="galdera" type="text" size="50" readonly /><br><br>
-				Erantzunak: <br/>  
-				<input type="radio" id="bat" name="erantzunak" value=""> 
-				<input type="radio" id="bi" name="erantzunak" value=""> 
-				<input type="radio" id="hiru" name="erantzunak" value=""> 
-				<input type="radio" id="lau" name="erantzunak" value="">
+				Erantzunak: <br/> 
+				<div id="erantz">
+
+				</div>
 				<br/><br/>
 				<input name="reset" type="reset" id="reset" value="Reset"/>
-				<input name="bidali" type="submit" id="bidali" value="Bidali"/>
+				<input name="bidali" type="button" id="bidali" value="Konprobatu" onClick="konprobatu()"/>
 	</form><br>
 
 
@@ -33,13 +53,15 @@
 include "dbConfig.php";
 global $niremysqli;
 
-$sql = "SELECT * FROM questions";
+$sql = "SELECT * FROM questions ORDER BY RAND()";
 $res = mysqli_query($niremysqli, $sql); 
 
 if (mysqli_num_rows($res) > 0) {
 	
-	echo rand(1, mysqli_num_rows($res));
+
+	
 }
+
 else{ echo "Ez dago lerrorik";}
 if (!$res){	echo("Errorea query-a gauzatzerakoan: ". mysqli_error($niremysqli));}
 
@@ -52,5 +74,31 @@ echo ("<a href=". $lay . "> ITZULI HASIERAKO ORRIRA </a></br></br>");
 
 //echo('<a href="../layout.html"> ITZULI HASIERAKO ORRIRA </a></br></br>');
 mysqli_close($niremysqli);
+
+
+function galderaBete($res){
+	$row = mysqli_fetch_assoc($res);
+	$galdera = $row['Galdera'];	
+	$zuzena = $row['Zuzena'];	
+	$okerra1 = $row['Okerra1'];	
+	$okerra2 = $row['Okerra2'];	
+	$okerra3 = $row['Okerra3'];		
+	echo "<script> $('#galdera').attr('value', '".$galdera."');</script>";
+	echo "<script> $('#bat').attr('value', '".$okerra1."');</script>";
+	echo "<script> $('#galdera').prepend('".$okerra1."');</script>";
+
+		
+	
+	echo "<script> $('#erantz').append(".'"'."<input type='radio' id='bat' name='erantzunak' value='".$zuzena."'>".$zuzena." <br/> ".'"'.");</script>";
+	echo "<script> $('#erantz').append(".'"'."<input type='radio' id='bat' name='erantzunak' value='".$okerra1."'>".$okerra1." <br/> ".'"'.");</script>";
+	echo "<script> $('#erantz').append(".'"'."<input type='radio' id='bat' name='erantzunak' value='".$okerra2."'>".$okerra2." <br/> ".'"'.");</script>";
+	echo "<script> $('#erantz').append(".'"'."<input type='radio' id='bat' name='erantzunak' value='".$okerra3."'>".$okerra3." <br/> ".'"'.");</script>";
+	echo "<script> var zuzena = '".$zuzena."';</script>";
+	
+	$arg = $row['Irudia'];	
+		if($row['Irudia']!="") echo "<script> $('#erantz').after('<img src=".$arg." width=100  />');</script>";
+	
+	
+}
 
 ?>
